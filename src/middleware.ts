dac8fee2ebe4976b646389bcea7131e1900a905c
NN-ACDM <nn.acdm@gmail.com>
2025-06-22 21:06:28 +0700

    [RL1] fix unsafe-inline on development

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,17 +1,19 @@
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
 function generateNonce(): string {
-  const array = new Uint8Array(16)
-  crypto.getRandomValues(array)
-  return Buffer.from(array).toString('base64')
+  const array = new Uint8Array(16);
+  crypto.getRandomValues(array);
+  return Buffer.from(array).toString("base64");
 }
 
-export function middleware(request: NextRequest) {
-  const nonce = generateNonce()
-  const response = NextResponse.next()
+const isDev = process.env.ENV == "development";
 
-  const csp = `
+export function middleware(request: NextRequest) {
+  const nonce = generateNonce();
+  const response = NextResponse.next();
+
+  let csp = `
     default-src 'self';
     script-src 'self' 'nonce-${nonce}';
     style-src 'self' 'nonce-${nonce}';
@@ -19,14 +21,29 @@ export function middleware(request: NextRequest) {
     img-src 'self' data:;
     connect-src 'self';
     object-src 'none';
-  `.replace(/\s{2,}/g, ' ').trim()
+    `
+    .replace(/\s{2,}/g, " ")
+    .trim();
+  if (isDev) {
+    csp = `
+    default-src 'self';
+    script-src 'self' 'unsafe-inline';
+    style-src 'self' 'unsafe-inline';
+    font-src 'self';
+    img-src 'self' data:;
+    connect-src 'self';
+    object-src 'none';
+    `
+      .replace(/\s{2,}/g, " ")
+      .trim();
+  }
 
-  response.headers.set('Content-Security-Policy', csp)
-  response.headers.set('x-nonce', nonce)
+  response.headers.set("Content-Security-Policy", csp);
+  response.headers.set("x-nonce", nonce);
 
-  return response
+  return response;
 }
 
 export const config = {
-  matcher: '/(.*)', // Apply to all routes
-}
+  matcher: "/(.*)", // Apply to all routes
+};
